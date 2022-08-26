8f5ef471d387506d6274b5c72274e19771a3df3c
chang <2622310479@qq.com>
2022-08-26 20:21:39 +0800

    chore: config Naive-ui theme style

--- components.d.ts
+++ components.d.ts
@@ -10,6 +10,7 @@ declare module '@vue/runtime-core' {
     Counter: typeof import('./src/components/Counter.vue')['default']
     Footer: typeof import('./src/components/Footer.vue')['default']
     NButton: typeof import('naive-ui')['NButton']
+    NConfigProvider: typeof import('naive-ui')['NConfigProvider']
     NSpace: typeof import('naive-ui')['NSpace']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
